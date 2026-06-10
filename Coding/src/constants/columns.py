91b61e5b8d4cab5befd8636d5a44af647075857b
PadTo <138981388+PadTo@users.noreply.github.com>
2026-06-10 17:43:20 +0200

    Create columns.py

---
+++ Coding/src/constants/columns.py
@@ -0,0 +1,21 @@
+# constants/columns.py
+
+USER_ID_COL = "user_id"
+ACTIVITY_DATE_COL = "activity_date"
+CHURN_ADJUSTED_DATE_COL = "churn_adjusted_date"
+CHURN_TRIGGERED_COL = "churn_triggered"
+VEHICLE_ID_COL = "vehicle_id"
+VEHICLE_MAKE_COL = "vehicle_make"
+VEHICLE_MODEL_COL = "vehicle_model"
+VEHICLE_START_YEAR_COL = "vehicle_start_year"
+VEHICLE_END_YEAR_COL = "vehicle_end_year"
+VEHICLE_MILEAGE_COL = "vehicle_mileage"
+APP_COL = "app"
+ACTIVITY_TYPE_COL = "activity_type"
+STILL_IN_PRODUCTION_COL = "still_in_production"
+INTERVAL_START_COL = "interval_start"
+
+# Year binning
+YEAR_BIN_LOWER: int = 2020
+YEAR_BIN_UPPER: int = 2026
+YEAR_BIN_UPPER_REPLACEMENT: int = 2025
