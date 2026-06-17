5c53e20c2763dfacb28830e8b11650de2a8a6433
PadTo <138981388+PadTo@users.noreply.github.com>
2026-06-17 12:56:54 +0200

    Adding constants files

---
+++ Coding/src/constants/cleaning.py
@@ -0,0 +1,12 @@
+NEXT_DATE_COL: str = "next_date"
+ACTIVITY_GAP_COL: str = "activity_gap"
+SHIFTED_DATE_COL: str = "activity_date_shifted"
+
+CSV_EXTENSION: str = ".csv"
+DEFAULT_OUTPUT_FILENAME: str = "no_name.csv"
+PERSONAL_USERS_FILENAME: str = "personal_users_dataset.csv"
+PROFESSIONAL_USERS_FILENAME: str = "professional_users_dataset.csv"
+
+DEFAULT_THRESHOLD_DAYS: int = 160
+MIN_ACTIVITY_SPAN_DAYS: int = 1
+FILL_VALUE_CHURN_TRIGGERED: bool = False
