b7cf36202a1a959fe5d38c97ccfebc1067f96c2a
PadTo <138981388+PadTo@users.noreply.github.com>
2026-06-18 10:36:48 +0200

    Update cleaning.py

--- Coding/src/constants/cleaning.py
+++ Coding/src/constants/cleaning.py
@@ -18,4 +18,4 @@ DEFAULT_CAR_SHARE_ABS: int          = 4
 DEFAULT_CAR_SHARE_FRACTION: float   = 0.8
 QUANTILE_FILTER: float              = 0.9
 BURST_TIME_HR: float                = 1/3
-MIN_ACTIVITY_SPAN_DAYS: int         = 1
+MIN_ACTIVITY_SPAN_DAYS: int         = 1 # This should remain 1 as it filters out one-day users
