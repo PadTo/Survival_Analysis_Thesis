85ecc52b24cca07e056ded3da0998dcb03692a40
PadTo <138981388+PadTo@users.noreply.github.com>
2026-07-12 19:44:47 +0300

    Changed grouping variables
    Changing grouping variables due to multicolinearity issues

--- Coding/src/constants/variables_groupings.py
+++ Coding/src/constants/variables_groupings.py
@@ -3,14 +3,14 @@ brand_group_map = {
     "audi": "audi",
     "skoda": "skoda",
     "seat": "seat",
+    "bmw": "bmw",
 
-    "bmw": "bmw_group",
-    "mini": "bmw_group",
-    "bmw moto": "bmw_group",
+    "mini": "other_rare",
+    "bmw moto": "other_rare",
 
-    "porsche": "premium",
-    "lamborghini": "premium",
-    "bentley": "premium",
+    "porsche": "other_rare",
+    "lamborghini": "other_rare",
+    "bentley": "other_rare",
 
     "mercedes-benz": "other_rare",
     "ford": "other_rare",
@@ -43,3 +43,76 @@ app_type_groups = {
     "main": "main",
     "vag": "vag"
 }
+
+
+
+
+
+
+pretty_old_features = {
+    "prop_clear_0_56": "Clear (prop)",
+    "prop_coding_0_56": "Coding (prop)",
+    "prop_history_screen_0_56": "History screen (prop)",
+    "prop_live_data_0_56": "Live data (prop)",
+    "prop_oca_0_56": "OCA (prop)",
+    "prop_scan_0_56": "Scan (prop)",
+    "prop_main_0_56": "Main app (prop)",
+    "prop_main_drift_0_28_vs_28_56": "Main app drift",
+    "n_sessions_0_28_days": "Sessions (28d)",
+    "sessions_intensity_drift_0_28_vs_28_56_days": "Session drift",
+    "n_actions_0_28_days": "Actions (28d)",
+    "actions_intensity_drift_0_28_vs_28_56_days": "Action drift",
+    "recency": "Recency",
+    "vehicle_mean_age_overall": "Mean vehicle age",
+    "prop_in_prod": "In production (prop)",
+    "overall_prop_vehicle_make_audi": "Audi (prop)",
+    "overall_prop_vehicle_make_bmw_group": "BMW group (prop)",
+    "overall_prop_vehicle_make_premium": "Premium (prop)",
+    "overall_prop_vehicle_make_seat": "SEAT (prop)",
+    "overall_prop_vehicle_make_skoda": "Škoda (prop)",
+    "overall_prop_vehicle_make_volkswagen": "VW (prop)"
+}
+
+
+
+
+
+pretty_new_features = {
+ 
+    "prop_clear_0_56": "Clear (prop)",
+    "prop_coding_0_56": "Coding (prop)",
+    "prop_history_screen_0_56": "History screen (prop)",
+    "prop_live_data_0_56": "Live data (prop)",
+    "prop_oca_0_56": "OCA (prop)",
+    "prop_scan_0_56": "Scan (prop)",
+
+ 
+    "prop_main_0_56": "Main app (prop)",
+    "prop_main_drift_0_28_vs_28_56": "Main app drift",
+
+ 
+    "n_sessions_0_28_days": "Sessions (28d)",
+    "sessions_intensity_drift_0_28_vs_28_56_days": "Session drift",
+    "actions_per_session_0_28_days": "Actions/session (28d)",
+    "actions_per_session_intensity_drift_0_28_vs_28_56_days": "Actions/session drift",
+
+  
+    "recency": "Recency",
+
+ 
+    "vehicle_mean_age_overall": "Mean vehicle age",
+    "prop_in_prod": "In production (prop)",
+    "overall_prop_vehicle_make_audi": "Audi (prop)",
+    "overall_prop_vehicle_make_bmw_group": "BMW group (prop)",
+    "overall_prop_vehicle_make_premium": "Premium (prop)",
+    "overall_prop_vehicle_make_seat": "SEAT (prop)",
+    "overall_prop_vehicle_make_skoda": "Škoda (prop)",
+    "overall_prop_vehicle_make_volkswagen": "VW (prop)",
+    "overall_prop_vehicle_make_other_rare": "Other rare (prop)",
+
+  
+    "CV_gap_0_56_days": "Gap CV (56d)",
+    "active_flag": "Active flag",
+}
+
+COLUMS_TO_EXCLUDE: list = ["user_id", "interval_start", "interval_end", "churn_triggered_adjusted"]
